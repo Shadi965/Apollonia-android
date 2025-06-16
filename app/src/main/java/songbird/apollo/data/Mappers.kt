7319640472a3c5dd0e6ca2e2b1e83632160ae7d5
Shadiev <shadiev.965@outlook.com>
2025-06-16 21:08:05 +0300

    Маппим данные из сети в вид для локальной базы

--- app/src/main/java/songbird/apollo/data/Mappers.kt
+++ app/src/main/java/songbird/apollo/data/Mappers.kt
@@ -1,6 +1,13 @@
 package songbird.apollo.data
 
+import songbird.apollo.data.local.SyncStatus
+import songbird.apollo.data.local.entity.AlbumEntity
+import songbird.apollo.data.local.entity.PlaylistEntity
+import songbird.apollo.data.local.entity.PositionedSong
+import songbird.apollo.data.local.entity.SongEntity
 import songbird.apollo.data.network.URL
+import songbird.apollo.data.network.dto.AlbumDto
+import songbird.apollo.data.network.dto.PlaylistDto
 import songbird.apollo.data.network.dto.SongDto
 import songbird.apollo.domain.model.Song
 import songbird.apollo.domain.model.SongPreview
@@ -20,6 +27,20 @@ fun SongDto.toSong() = Song(
     coverUrl = "$URL/album/cover/${albumId}"
 )
 
+fun SongDto.toEntity() = SongEntity(
+    id,
+    title,
+    artist,
+    composer,
+    albumId,
+    track,
+    disc,
+    date,
+    copyright,
+    genre,
+    duration
+)
+
 fun SongDto.toSongPreview() = SongPreview(
     id,
     title,
@@ -27,4 +48,30 @@ fun SongDto.toSongPreview() = SongPreview(
     albumId,
     position = 0.0,
     coverUrl = "$URL/album/cover/${albumId}"
+)
+
+fun PositionedSong.toSongPreview() = SongPreview(
+    song.id,
+    song.title,
+    song.artist,
+    song.albumId,
+    position,
+    coverUrl = "$URL/album/cover/${song.albumId}"
+)
+
+fun AlbumDto.toEntity() = AlbumEntity(
+    id,
+    title,
+    artist,
+    trackCount,
+    discCount,
+    date,
+    copyright,
+    genre
+)
+
+fun PlaylistDto.toEntity(syncStatus: SyncStatus) = PlaylistEntity(
+    id,
+    name,
+    syncStatus = syncStatus
 )
