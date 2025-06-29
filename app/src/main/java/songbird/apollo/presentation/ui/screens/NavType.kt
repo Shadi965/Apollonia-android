f92cf70071f2d5fdfa63df2a7cb3d479ede3d6f4
Shadiev <shadiev.965@outlook.com>
2025-06-29 18:36:24 +0300

    пусть так побудет

--- app/src/main/java/songbird/apollo/presentation/ui/screens/NavType.kt
+++ app/src/main/java/songbird/apollo/presentation/ui/screens/NavType.kt
@@ -1,23 +1,23 @@
 package songbird.apollo.presentation.ui.screens
 
-import android.os.Bundle
-import androidx.navigation.NavType
-import kotlinx.serialization.json.Json
-import songbird.apollo.presentation.model.SongPreviewUi
-import kotlin.reflect.typeOf
-
-object SongPreviewUiNavType : NavType<SongPreviewUi>(isNullableAllowed = false) {
-    override fun get(bundle: Bundle, key: String): SongPreviewUi? {
-        return bundle.getString(key)?.let { Json.decodeFromString<SongPreviewUi>(it) }
-    }
-
-    override fun parseValue(value: String): SongPreviewUi {
-        return Json.decodeFromString(value)
-    }
-
-    override fun put(bundle: Bundle, key: String, value: SongPreviewUi) {
-        bundle.putString(key, Json.encodeToString(value))
-    }
-}
-
-val navTypeMap = mapOf(typeOf<SongPreviewUi>() to SongPreviewUiNavType)
+//import android.os.Bundle
+//import androidx.navigation.NavType
+//import kotlinx.serialization.json.Json
+//import songbird.apollo.presentation.model.SongPreviewUi
+//import kotlin.reflect.typeOf
+//
+//object SongPreviewUiNavType : NavType<SongPreviewUi>(isNullableAllowed = false) {
+//    override fun get(bundle: Bundle, key: String): SongPreviewUi? {
+//        return bundle.getString(key)?.let { Json.decodeFromString<SongPreviewUi>(it) }
+//    }
+//
+//    override fun parseValue(value: String): SongPreviewUi {
+//        return Json.decodeFromString(value)
+//    }
+//
+//    override fun put(bundle: Bundle, key: String, value: SongPreviewUi) {
+//        bundle.putString(key, Json.encodeToString(value))
+//    }
+//}
+//
+//val navTypeMap = mapOf(typeOf<SongPreviewUi>() to SongPreviewUiNavType)
